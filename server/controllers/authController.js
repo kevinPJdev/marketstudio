a4e077a7cd1950fff12c73382447cd464760964d
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2022-10-11 20:42:04 -0400

    Update authController

---
+++ server/controllers/authController.js
@@ -0,0 +1,56 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const config = require('config');
+const User = require('../models/User');
+
+const signup = (req, res) => {
+  const {name, email, password} = req.body;
+
+  if(!name || !email || !password) {
+    return res.status(400).json({msg: "Please enter all fields!"});
+  }
+
+  User.findOne({email})
+    .then(user => {
+      if(user) {
+        return res.status(400).json({msg: "User already exists"});
+      }
+
+      const newUser = new User({name, email, password});
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(password, salt, (err, hash) => {
+          if (err) throw new Error('Problem with hashing. Please check.');
+          newUser.password = hash;
+          newUser.save()
+            .then(user => {
+              jwt.sign(
+                {id: user._id},
+                config.get('jwtsecret'),
+                {expiresIn: 3600},
+                (err, token) => {
+                  if (err) throw new Error('Problem with JWT token generation. Please check.');
+                  res.json({
+                    token,
+                    user: {
+                      id: user._id,
+                      name: user.name,
+                      email: user.email
+                    }
+                  })
+                }
+              )
+            })
+        })
+      }) 
+    })
+}
+
+const login = (req, res) => {
+  const {email, password} = req.body;
+
+  if(!email || !password) {
+    res.status(400).json({msg: "Please enter all login fields"});
+  }
+}
