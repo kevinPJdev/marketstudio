a18fbd9d9f7e0efb8ce314a2981596c1115c1bff
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2022-10-14 00:31:53 -0400

    update cartController

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,4 +1,5 @@
 const Cart = require('../models/Cart');
+const Product = require('../models/Product');
 
 const getCartItems = (req, res) => {
   Cart.findOne({userId: req.params.id})
@@ -15,10 +16,76 @@ const getCartItems = (req, res) => {
     })
 }
 
-const postCardItems = (req, res) => {
+const postCartItems = async(req, res) => {
   const { productId, quantity } = req.body;
 
-  Cart.findOne
+  try {
+    let cart = await Cart.findOne({userId: req.params.id});
+    let product = await Product.findOne({_id: productId});
+    if(!product) {
+      res.status(400).json({msg: "Product not found"});
+    }
+    const name = product.productName;
+    const price = product.price;
+
+    if(cart) {
+      //add an item to cart
+      let productIndex = cart.products.findIndex(p => p.productId === productId);
+
+      if(productIndex > -1) {
+        let productItem = cart.products[productIndex];
+        productItem.quantity += quantity;
+        cart.products[productIndex] = productItem;
+      } else {
+        cart.products.push({productId, name, quantity, price});
+      }
+      cart.bill += quantity*price;
+      cart = await cart.save();
+      return res.status(201).send(cart);
+    } 
+    else {
+      //create a new cart
+      const newCart = Cart.create({
+        userId,
+        products: [{productId, name, quantity, price}],
+        bill: quantity * price
+      });
+      return res.status(201).send(newCart);
+    }
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({msg: "Something went wrong!"});
+  }
 }
 
-module.exports = {getCartItems}
+const deleteItem = async(req, res) => {
+  const productId = req.params.id;
+  const userId = req.params.userId;
+
+  //check if the item exists in cart
+  try {
+    let cart = await Cart.findOne({userId: userId});
+
+    if(cart) {
+      const productIndex = cart.products.findIndex(p => p.productId === productId);
+
+      if(productIndex > -1) {
+        let productItem = cart.products[productIndex];
+        cart.bill -= productItem.quantity * productItem.price;
+        cart.products.splice(productIndex, 1);
+      } else {
+        //Item does not exist in cart
+        return res.status(400).send({msg: "Product does not exist in this cart"});
+      }
+      cart = await cart.save();
+      return res.status(201).send(cart);
+    } 
+    else {
+      return res.status(400).json({msg: "Cart was not found"});
+    }
+  } catch(err) {
+    return res.status(500).json({msg: "Something went bad"});
+  }
+}
+
+module.exports = {getCartItems, postCartItems, deleteItem}
