c6c5503b13c9c9c0395680086246d11a119d26cb
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2022-10-14 00:33:38 -0400

    update cartRoute

--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const cartController = require('../controllers/cartController');
 
 router.get('/cart/:id', cartController.getCartItems);
-router.post('/cart/:id', cartController.postCardItems);
+router.post('/cart/:id', cartController.postCartItems);
 router.delete('cart/userId/:productId', cartController.deleteItem);
 
 module.exports = router;
