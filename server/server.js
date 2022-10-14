a4346cef871a8a5caf0088dccbd512db9eb529b6
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2022-10-14 01:40:17 -0400

    fixed extension bugs and added routes to server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,18 @@ const mongoose = require('mongoose');
 const path = require('path');
 const config = require('config');
 
+const authRoutes = require('./routes/authRoute');
+const productRoutes = require('./routes/productRoute');
+const cartRoutes = require('./routes/cartRoute');
+const orderRoutes = require('./routes/orderRoute');
+
 const app = express();
 app.use(express.json());
+
+app.use('/api',authRoutes);
+app.use('/api',productRoutes);
+app.use('/api',cartRoutes);
+app.use('/api',orderRoutes);
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
@@ -20,3 +30,4 @@ mongoose.connect(dbURI,  {useNewUrlParser: true, useUnifiedTopology: true})
   .then((result) => app.listen(port))
   .then(() => console.log("Server is listening on port ", port ))
   .catch((err) => console.log(err));
+
